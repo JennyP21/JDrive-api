894b71e26254daaccfbf4cdfd1c179dac3abd1cd
Jenny Patel <pateljenny555@gmail.com>
2023-04-24 11:12:03 -0700

    Updated Files Table

--- models/createFileTable.js
+++ models/createFileTable.js
@@ -2,8 +2,8 @@ const db = require("../startup/db");
 
 module.exports = function () {
   let fileTable = `
-CREATE TABLE IF NOT EXISTS files(
-    id CHAR(36) NOT NULL,
+  CREATE TABLE files (
+    id char(36) NOT NULL,
     name longtext,
     type tinytext,
     size int DEFAULT NULL,
@@ -11,8 +11,12 @@ CREATE TABLE IF NOT EXISTS files(
     localPath longtext,
     created date DEFAULT NULL,
     modified date DEFAULT NULL,
+    owner char(36) DEFAULT NULL,
+    trashed tinyint DEFAULT '0',
     PRIMARY KEY (id),
-    UNIQUE KEY id_UNIQUE (id)
+    UNIQUE KEY id_UNIQUE (id),
+    KEY owner_idx (owner),
+    CONSTRAINT owner FOREIGN KEY (owner) REFERENCES account (id)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 `;
 
