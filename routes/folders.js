d3eddec71d95ac1f300b690d1a93da6fcc95feb1
Jenny Patel <pateljenny555@gmail.com>
2023-04-24 13:48:22 -0700

    Creating API to add Folder

--- routes/folders.js
+++ routes/folders.js
@@ -4,7 +4,17 @@ const router = express.Router();
 const db = require("../startup/db");
 
 // Creating folder
-router.post("/create", (req, res) => {});
+router.post("/create", (req, res) => {
+  const currentFolder = req.body.currentFolder;
+  const folderName = req.body.folderName;
+  const id = crypto.randomUUID();
+  const createFolder =
+    "INSERT INTO FOLDERS(id, name, parentFolder) VALUES(?, ?, ?)";
+
+  db.query(createFolder, [id, folderName, currentFolder], (error, results) => {
+    if (error) return error;
+  });
+});
 
 // Deleting folder
 router.get("/delete", (req, res) => {});
