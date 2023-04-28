7b5f0f4d3b3254378c345be9e45603b4676b996f
Jenny Patel <pateljenny555@gmail.com>
2023-04-28 15:17:31 -0700

    Added Get methods to Folder Route

--- routes/folders.js
+++ routes/folders.js
@@ -3,6 +3,24 @@ const express = require("express");
 const router = express.Router();
 const db = require("../startup/db");
 
+// Get Folder
+router.get("/", (req, res) => {
+  const getFolder = "SELECT * FROM FOLDERS";
+  db.query(getFolder, (error, results) => {
+    if (error) res.send("Something went wrong").status(500);
+    res.send(results);
+  });
+});
+
+// Get Root Folder
+router.get("/mydrive", (req, res) => {
+  const getFolder = "SELECT * FROM FOLDERS WHERE parentFolder IS NULL";
+  db.query(getFolder, (error, results) => {
+    if (error) res.send("Something went wrong").status(500);
+    res.send(results);
+  });
+});
+
 // Creating folder
 router.post("/create", (req, res) => {
   const id = crypto.randomUUID();
