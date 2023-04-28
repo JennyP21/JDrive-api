f2c28b7406d98f3340026119029993f04c445b57
Jenny Patel <pateljenny555@gmail.com>
2023-04-28 10:58:33 -0700

    Creating Default Folder as User Account is Created

--- routes/auth.js
+++ routes/auth.js
@@ -5,18 +5,36 @@ const db = require("../startup/db");
 
 // Creating Account
 router.post("/register", (req, res) => {
-  const id = crypto.randomUUID();
+  const ownerID = crypto.randomUUID();
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
   const createAccount =
     "INSERT INTO ACCOUNTS(id, name, email, password) VALUES(?, ?, ?, ?)";
 
-  db.query(createAccount, [id, name, email, password], (error, results) => {
-    if (error) return error;
-    console.log(results);
-  });
-  res.send("Account create successfully");
+  db.query(
+    createAccount,
+    [ownerID, name, email, password],
+    (error, results) => {
+      if (error) return error;
+    }
+  );
+
+  const folderId = crypto.randomUUID();
+  const defaultFolderName = "mydrive";
+
+  const createDefaultFolder =
+    "INSERT INTO FOLDERS(id, name, folderOwner) VALUES(?, ?, ?)";
+
+  db.query(
+    createDefaultFolder,
+    [folderId, defaultFolderName, ownerID],
+    (error, results) => {
+      if (error) return error;
+    }
+  );
+
+  res.send("Account created successfully");
 });
 
 router.get("/", (req, res) => {
